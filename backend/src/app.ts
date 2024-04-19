7acc3ef2e0cb3930198180c9869162972a3353cb
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-19 13:24:23 +0100

    chore: added mongodb, installed cors and connected to database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,27 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose";
+import cors from "cors"
+
+dotenv.config();
+
+const mongoDbUrl = `mongodb+srv://sidra1360:${process.env.MONGO_PW}@cluster0.xi8zihz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const app = express();
+
+app.use(cors());
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoDbUrl);
+    console.log('MongoDB is Connected!')
+  } catch (error) {
+    console.log('Unable to connect to MongoDB Database!');
+    console.error(error);
+  }
+}
+
+connectToDatabase();
 
 app.use(express.json());
 
