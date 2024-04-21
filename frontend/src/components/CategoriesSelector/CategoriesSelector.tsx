f995084499438d979f77a4c9ff266dc3c56edc0e
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-21 15:35:12 +0100

    chore: setup of components for News Feed

---
+++ frontend/src/components/CategoriesSelector/CategoriesSelector.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const CategoriesSelector = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoriesSelector
---
+++ frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const SignUpBenefits = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignUpBenefits
