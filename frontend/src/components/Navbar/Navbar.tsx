556d2bc4835bee4223a72f9da8c6a8c90a55a8a7
Pablo <pablito@me.com>
2024-04-21 16:20:33 +0100

    Some basic structural navbar css with some dummy content.

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,7 +3,21 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav>
-      Hello
+      <div className="navbar">
+        <div className="nav-logo-title">
+          <img className="nav-logo" src="/images/fixit-logo.png" alt="fixit-logo" />
+          <h1>FixIt</h1>
+        </div>
+        <div className="nav-search">
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="nav-icons">
+          <img className="nav-logo" src="/images/fixit-logo.png" alt="fixit-logo" />
+          <img className="nav-logo" src="/images/fixit-logo.png" alt="fixit-logo" />
+          <img className="nav-logo" src="/images/fixit-logo.png" alt="fixit-logo" />
+          <img className="nav-logo" src="/images/fixit-logo.png" alt="fixit-logo" />
+        </div>
+      </div>
     </nav>
   )
 }
