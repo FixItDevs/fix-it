e3ecc21b27fb57edeb2b4657d55c578d73a3eacd
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-21 13:56:03 +0100

    chore: user Schema completed

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userModel = new mongoose.Schema({
   username: {
@@ -11,4 +12,26 @@ const userModel = new mongoose.Schema({
   },
   email: { type: String, required: true},
   password: { type: String, required: true}
-})
+});
+
+userModel.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+
+    this.password = hashedPassword;
+
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+const User = mongoose.model('User', userModel);
+
+export default User;
