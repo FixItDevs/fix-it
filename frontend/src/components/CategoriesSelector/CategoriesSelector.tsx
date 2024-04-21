06f84ae5fc3d4484e618baf0fa2130497f4c145d
Pablo <pablito@me.com>
2024-04-21 16:57:01 +0100

    Basic layout of left and right cols.

--- frontend/src/components/CategoriesSelector/CategoriesSelector.tsx
+++ frontend/src/components/CategoriesSelector/CategoriesSelector.tsx
@@ -1,11 +1,22 @@
-import "./CategoriesSelector.css"
+import "./CategoriesSelector.css";
 
 const CategoriesSelector = () => {
   return (
     <div className="category-selector-box">
-      Hello
+      <div className="categories-title-box">
+        <h1 className="categories-title">Trending Topics</h1>
+      </div>
+      <div className="categories-list-box">
+        <div className="category-item">Plumbing</div>
+        <div className="category-item">Electrical</div>
+        <div className="category-item">Appliances</div>
+        <div className="category-item">Decorating</div>
+        <div className="category-item">Roofing</div>
+        <div className="category-item">Carpentry</div>
+        <div className="category-item">Metalwork</div>
+      </div  >
     </div>
-  )
-}
+  );
+};
 
-export default CategoriesSelector
+export default CategoriesSelector;
--- frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
+++ frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
@@ -4,7 +4,8 @@ import './SignUpBenefits.css'
 const SignUpBenefits = () => {
   return (
     <div className="sign-up-benefits-container">
-      Hello
+      Hello. I am the column on the right and nobody cares about me 😞.
+      I'm not even good enough for dummy text.
     </div>
   )
 }
