21986139de49d2865f19976fa6825e078e04899b
Pablo <pablito@me.com>
2024-04-22 13:02:25 +0100

    Added single user data.

---
+++ backend/src/seeds/userSeedData.ts
@@ -0,0 +1,11 @@
+export const userSeedData = [
+  {
+    username: 'bob',
+    avatar: {
+      url: 'https://assets-prd.ignimgs.com/2024/01/25/bob-the-builder-button-1706221996136.jpg'
+    },
+    email: 'bob@example.com',
+    password: 'Password123'
+  },
+];
+
