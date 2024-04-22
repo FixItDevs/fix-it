8909ff0e5f62501701be58564320dcccee48a65f
Pablo <pablito@me.com>
2024-04-22 10:06:43 +0100

    Uncommented User property in the Post Model.

--- backend/src/models/postModel.ts
+++ backend/src/models/postModel.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const postModel = new mongoose.Schema({
-  // user: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User',
-  //   required: true
-  // },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   postText: { 
     title: { type: String},
     description: { type: String }
