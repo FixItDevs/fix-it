5bcaae93352368636e243078cf6bc93ce47bc938
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-22 11:51:59 +0100

    chore: added email validation

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,15 +2,24 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/userModel';
 
 const validatePassword = (password: string) => {
-  const regex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{8,}$/;
+  const regex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._£-]{8,}$/;
   return regex.test(password);
+}
+
+const validateEmail = (email: string) => {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
 }
 
 export const addUser = async (req: Request, res: Response, next: NextFunction) => {
   const { username, avatar, email, password } = req.body;
 
   if (!validatePassword(password)) {
-    return res.status(400).json({ message: 'Invalid password format' });
+    return res.status(400).json({ message: 'Invalid password format, please try again.' });
+  }
+
+  if (!validateEmail(email)) {
+    return res.status(400).json({ message: 'Invalid email, please try again.' });
   }
 
   try {
