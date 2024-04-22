93e8cd926022a4bf9690631d29d9d796c361605c
Pablo <pablito@me.com>
2024-04-22 18:48:13 +0100

    Updated list of categories.

--- frontend/src/data/categoriesList.ts
+++ frontend/src/data/categoriesList.ts
@@ -1,9 +1,30 @@
 export const categoriesList = [
-  "Plumbing",
-  "Electrical",
-  "Appliances",
-  "Decorating",
-  "Roofing",
-  "Carpentry",
-  "Metalwork"
+  {
+    main: "Plumbing",
+    sub: ["Drainage", "Heating", "Pipes", "Toilet", "Taps", "Showers", "Plumbing repairs", "New plumbing installations", "Waste", "Soldering", "Compression"]
+  },
+  {
+    main: "Electrical",
+    sub: ["Lighting", "Cabling", "Mains", "Consumer unit", "Sockets", "Switches", "Electrical repairs", "New electrical installations", "Earthing", "Bonding", "Testing"]
+  },
+  {
+    main: "Appliances",
+    sub: ["Washing Machine", "Dishwasher", "Fridge", "Oven", "Hob", "Extractor", "Microwave", "Tumble Dryer", "Freezer", "Waste Disposal", "Water Softener", "Water Heater"]
+  },
+  {
+    main: "Decorating",
+    sub: ["Painting", "Wallpaper", "Tiling", "Flooring", "Furniture", "Plastering", "Coving", "Skirting", "Architrave", "Staining", "Varnishing", "Sealing"]
+  },
+  {
+    main: "Roofing",
+    sub: ["Tiles", "Slates", "Flashing", "Gutters", "Chimney", "Fascia", "Soffit", "Leadwork", "Flat Roofing", "Pitched Roofing", "Roof Repairs", "New Roofing"]
+  },
+  {
+    main: "Carpentry",
+    sub: ["Doors", "Windows", "Furniture", "Flooring", "Decking", "Fencing", "Stairs", "Shelving", "Cupboards", "Worktops", "Skirting", "Architrave"]
+  },
+  {
+    main: "Metalwork",
+    sub: ["Welding", "Cutting", "Blacksmithing", "Forging", "Casting", "Polishing", "Grinding", "Drilling", "Tapping", "Bending", "Soldering", "Riveting"]
+  }
 ];
