044a622a22d0a204c36525452a41e04b91bfff58
Pablo <pablito@me.com>
2024-04-24 20:17:19 +0100

    Ammended types for new post model.

--- backend/src/models/postModel.ts
+++ backend/src/models/postModel.ts
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
 const postModel = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+  postAuthor: {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    }
   },
   postText: { 
     title: { type: String},
-    description: { type: String }
+    body: { type: String }
   },
   tags: {
     mainTags: [String],
@@ -25,11 +35,37 @@ const postModel = new mongoose.Schema({
 
   }],
   comments: [{
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    comment: { type: String },
+    commentAuthor: {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String,
+        required: true
+      }
+    },
+    commentText: { type: String },
     replies: [{
-      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-      comment: { type: String }
+      replyAuthor: {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        },
+        username: {
+          type: String,
+          required: true
+        },
+        avatar: {
+          type: String,
+          required: true
+        }
+      },
+      replyText: { type: String }
     }]
   }],
   votes: [{
