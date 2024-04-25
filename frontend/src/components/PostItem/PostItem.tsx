8b8f4f8f785922f86c5f3ecec3488a629e7f7842
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-25 15:45:57 +0100

    chore: linter

--- frontend/src/components/PostItem/PostItem.tsx
+++ frontend/src/components/PostItem/PostItem.tsx
@@ -18,7 +18,6 @@ const PostItem: React.FC<PostObject> = ({
   votes,
   createdAt
 }) => {
-
   const renderVoteSection = (votes: Vote[]) => {
     let upvoteCount = 0;
     let downvoteCount = 0;
@@ -66,73 +65,96 @@ const PostItem: React.FC<PostObject> = ({
 
   return (
     <div className="post-container">
-      {renderAvatarUsernameSection(postAuthor.username, postAuthor.avatar, createdAt)}
+      {renderAvatarUsernameSection(
+        postAuthor.username,
+        postAuthor.avatar,
+        createdAt
+      )}
       <h2 className="post-title">{postText.title}</h2>
       <p className="post-description">{postText.body}</p>
       <p className="post-main-tags">{tags.mainTags}</p>
       <p className="post-sub-tag">{tags.subTag}</p>
-    <div className="post">
-      <h2 className="post-title">{postText.title}</h2>
-      <p className="post-description">{postText.body}</p>
-      <br />
-      <div className="user-and-avatar">
-        <p className="post-user">By {postAuthor.username}</p>
-        <img src={postAuthor.avatar} alt="avatar" className="user-avatar" />
-      </div>
-      <br />
-      <p className="post-main-tags">Tags: {tags.mainTags.join(', ')} and {tags.subTag.join(', ')}</p>
-      <br />
-      <div>
-        {comments?.length > 0 && <h4>Comments:</h4>}
-        {comments?.map((comment, index) => (
-          <div key={index}>
-            <div className="user-and-avatar">
-            <p>{comment.commentText} - {comment.commentAuthor.username}</p>
-              <img src={comment.commentAuthor.avatar} alt="avatar" className="user-avatar" />
-            </div>
-            {comment.replies?.map((reply, index) => (
-              <div key={index}>
-                <div className="user-and-avatar">
-                  <p> --- {reply.replyText} - {reply.replyAuthor.username}</p>
-                  <img src={reply.replyAuthor.avatar} alt="avatar" className="user-avatar" />
-          </div>
-          </div>
-            ))}
-            <br />
-          </div>
-        ))}
-      </div>
-      <div>
-        {images?.map((image, index) => (
-          <div key={index}>
-            <img src={image.url} alt={image.caption} style={{ width: '300px' }} />
-          </div>
-        ))}
-      </div>
-      <br />
-      <div>
-        {videos?.map((video, index) => (
-          <div key={index}>
-            <p>This video does not work for some reason.</p>
-          <iframe
-            width="300"
-            height="200"
-            src='https://www.youtube.com/watch?v=1SVv4RuGWYk'
-            title={video.caption}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            ></iframe>
-          <p>{video.caption}</p>
-        </div>
-        ))}
-      </div>
+      <div className="post">
+        <h2 className="post-title">{postText.title}</h2>
+        <p className="post-description">{postText.body}</p>
         <br />
-      <div className="post-votes-and-comments-section">
-        {renderVoteSection(votes || [])}
-        {renderCommentSection(comments || [])}
+        <div className="user-and-avatar">
+          <p className="post-user">By {postAuthor.username}</p>
+          <img src={postAuthor.avatar} alt="avatar" className="user-avatar" />
+        </div>
+        <br />
+        <p className="post-main-tags">
+          Tags: {tags.mainTags.join(", ")} and {tags.subTag.join(", ")}
+        </p>
+        <br />
+        <div>
+          {comments?.length > 0 && <h4>Comments:</h4>}
+          {comments?.map((comment, index) => (
+            <div key={index}>
+              <div className="user-and-avatar">
+                <p>
+                  {comment.commentText} - {comment.commentAuthor.username}
+                </p>
+                <img
+                  src={comment.commentAuthor.avatar}
+                  alt="avatar"
+                  className="user-avatar"
+                />
+              </div>
+              {comment.replies?.map((reply, index) => (
+                <div key={index}>
+                  <div className="user-and-avatar">
+                    <p>
+                      {" "}
+                      --- {reply.replyText} - {reply.replyAuthor.username}
+                    </p>
+                    <img
+                      src={reply.replyAuthor.avatar}
+                      alt="avatar"
+                      className="user-avatar"
+                    />
+                  </div>
+                </div>
+              ))}
+              <br />
+            </div>
+          ))}
+        </div>
+        <div>
+          {images?.map((image, index) => (
+            <div key={index}>
+              <img
+                src={image.url}
+                alt={image.caption}
+                style={{ width: "300px" }}
+              />
+            </div>
+          ))}
+        </div>
+        <br />
+        <div>
+          {videos?.map((video, index) => (
+            <div key={index}>
+              <p>This video does not work for some reason.</p>
+              <iframe
+                width="300"
+                height="200"
+                src="https://www.youtube.com/watch?v=1SVv4RuGWYk"
+                title={video.caption}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              <p>{video.caption}</p>
+            </div>
+          ))}
+        </div>
+        <br />
+        <div className="post-votes-and-comments-section">
+          {renderVoteSection(votes || [])}
+          {renderCommentSection(comments || [])}
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
