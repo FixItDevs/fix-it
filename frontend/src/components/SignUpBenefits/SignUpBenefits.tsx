a7ac56d4b1cc37f543d0482afc4a60789fd8671b
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-25 10:28:51 +0100

    feat: core benefits added

--- frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
+++ frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
@@ -1,21 +1,23 @@
 // import React from 'react'
 import "./SignUpBenefits.css";
-import HardwareIcon from '@mui/icons-material/Hardware';
+import HardwareIcon from "@mui/icons-material/Hardware";
 
 const SignUpBenefits = () => {
-
   const benefits = [
     "Learn from over 50 DIY topics",
     "Earn reputation by helping others",
     "Build a FixIt community",
     "Make your DIY project dream a reality! "
-  ]
+  ];
 
   return (
     <div className="sign-up-benefits-container">
       <span>Why not sign up today?</span>
       {benefits.map((benefit) => (
-        <div key={benefit}><HardwareIcon/>{benefit}</div>
+        <div key={benefit}>
+          <HardwareIcon />
+          {benefit}
+        </div>
       ))}
     </div>
   );
