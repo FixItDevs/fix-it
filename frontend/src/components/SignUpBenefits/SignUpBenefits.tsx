86677da44ce028fd1390c96308dfb979d059bc20
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-25 10:28:37 +0100

    feat: core benefits added

--- frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
+++ frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
@@ -1,11 +1,22 @@
 // import React from 'react'
 import "./SignUpBenefits.css";
+import HardwareIcon from '@mui/icons-material/Hardware';
 
 const SignUpBenefits = () => {
+
+  const benefits = [
+    "Learn from over 50 DIY topics",
+    "Earn reputation by helping others",
+    "Build a FixIt community",
+    "Make your DIY project dream a reality! "
+  ]
+
   return (
     <div className="sign-up-benefits-container">
-      Hello. I am the column on the right and nobody cares about me 😞. I'm not
-      even good enough for dummy text.
+      <span>Why not sign up today?</span>
+      {benefits.map((benefit) => (
+        <div key={benefit}><HardwareIcon/>{benefit}</div>
+      ))}
     </div>
   );
 };
