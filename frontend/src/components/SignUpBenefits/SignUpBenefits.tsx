75fd176bf84044dd026125da5cc5c44b1da47609
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-25 13:48:22 +0100

    chore: sign up pointer

--- frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
+++ frontend/src/components/SignUpBenefits/SignUpBenefits.tsx
@@ -10,9 +10,13 @@ const SignUpBenefits = () => {
     "Make your DIY project dream a reality! "
   ];
 
+  const handleSignUpClick = () => {
+    console.log('Right hand - Sign up clicked')
+  }
+
   return (
     <div className="sign-up-benefits-container">
-      <span className="sign-up-title">Sign up today!</span>
+      <span className="sign-up-title" onClick={() => handleSignUpClick()}>Sign up today!</span>
       {benefits.map((benefit) => (
         <div key={benefit} className="sign-up-benefit-encasing">
           <div className="hardware-icon">
