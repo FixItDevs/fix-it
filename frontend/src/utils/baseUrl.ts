9be874e969e830e1ef08e6f4f98653d1148fa485
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-27 12:19:20 +0100

    chore: linter

--- frontend/src/utils/baseUrl.ts
+++ frontend/src/utils/baseUrl.ts
@@ -1,5 +1,6 @@
-const baseUrl: string = process.env.NODE_ENV === 'deploy'
-  ? 'http://localhost:3000' // Yes, makes no sense now but will be needed for deployment
-  : 'http://localhost:3000';
+const baseUrl: string =
+  process.env.NODE_ENV === "deploy"
+    ? "http://localhost:3000" // Yes, makes no sense now but will be needed for deployment
+    : "http://localhost:3000";
 
 export default baseUrl;
