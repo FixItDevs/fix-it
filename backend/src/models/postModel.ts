6f9f6ff74343d2e13d24f0c859571cd987afa065
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-27 12:18:14 +0100

    feat: updated post comment and reply schema with timestamps

--- backend/src/models/postModel.ts
+++ backend/src/models/postModel.ts
@@ -1,5 +1,46 @@
 import mongoose from "mongoose";
 
+const replySchema = new mongoose.Schema({
+  replyAuthor: {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    }
+  },
+  replyText: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+const commentSchema = new mongoose.Schema({
+  commentAuthor: {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    }
+  },
+  commentText: { type: String },
+  replies: [replySchema],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
 const postModel = new mongoose.Schema({
   postAuthor: {
     userId: {
@@ -34,40 +75,7 @@ const postModel = new mongoose.Schema({
     caption: { type: String, required: false }
 
   }],
-  comments: [{
-    commentAuthor: {
-      userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      },
-      username: {
-        type: String,
-        required: true
-      },
-      avatar: {
-        type: String,
-        required: true
-      }
-    },
-    commentText: { type: String },
-    replies: [{
-      replyAuthor: {
-        userId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'User'
-        },
-        username: {
-          type: String,
-          required: true
-        },
-        avatar: {
-          type: String,
-          required: true
-        }
-      },
-      replyText: { type: String }
-    }]
-  }],
+  comments: [commentSchema],
   votes: [{
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     type: { type: String, enum: ['upvote', 'downvote']}
