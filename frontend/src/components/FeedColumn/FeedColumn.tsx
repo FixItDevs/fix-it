137182f07f5fb7d605bd88c9d1d87eb6d262c917
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-05-01 13:34:04 +0100

    fix: remove non working infinite scroll

--- frontend/src/components/FeedColumn/FeedColumn.tsx
+++ frontend/src/components/FeedColumn/FeedColumn.tsx
@@ -3,78 +3,52 @@ import PostItem from "../PostItem/PostItem";
 import { useEffect, useState } from "react";
 import { PostObject } from "../../types/post.types";
 import axios from "axios";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 const FeedColumn = () => {
   const [feedPosts, setFeedPosts] = useState<PostObject[]>([]);
-  const [hasMore, setHasMore] = useState(true);
 
   async function getPosts() {
-    axios
-      .get("http://localhost:3000/api/v1.0/posts", {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1.0/posts", {
         params: {
-          _limit: 5
+          _limit: 40
         }
-      })
-      .then((res) => {
-        const posts = res.data;
-        console.log("getPosts:", posts);
-        setFeedPosts(posts);
-      })
-      .catch((error) => console.log(error));
+      });
+
+      if (response.status !== 200) {
+        throw new Error("Error in network response");
+      }
+      const posts = response.data;
+      setFeedPosts(posts);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
   }
 
   useEffect(() => {
     getPosts();
   }, []);
 
-  const fetchMorePostData = () => {
-    axios
-      .get(`http://localhost:3000/api/v1.0/posts`, {
-        params: {
-          offset: feedPosts.length,
-          _limit: 5
-        }
-      })
-      .then((res) => {
-        if (res.data.length > 0) {
-          setFeedPosts((prevPosts) => [...prevPosts, ...res.data]);
-          console.log("fetchMorePosts:", feedPosts);
-        } else {
-          setHasMore(false);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-
   return (
     <div className="feed-col-container">
-      <h1 className="feed-title">I am a dummy title by the way</h1>
-
+      <h1 className="feed-title">FixIt news feed</h1>
       <div className="main-feed-container">
-        <InfiniteScroll
-          dataLength={feedPosts.length}
-          next={fetchMorePostData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-        >
-          {feedPosts.map((post: PostObject) => (
-            <div key={post.postId}>
-              <PostItem
-                postId={post.postId}
-                postAuthor={post.postAuthor}
-                postText={post.postText}
-                tags={post.tags}
-                comments={post.comments}
-                images={post.images}
-                videos={post.videos}
-                votes={post.votes}
-                createdAt={post.createdAt}
-              />
-              <div className="horizontal-divider feed-divider"></div>
-            </div>
-          ))}
-        </InfiniteScroll>
+        {feedPosts.map((post: PostObject) => (
+          <div key={post.postId}>
+            <PostItem
+              postId={post.postId}
+              postAuthor={post.postAuthor}
+              postText={post.postText}
+              tags={post.tags}
+              comments={post.comments}
+              images={post.images}
+              videos={post.videos}
+              votes={post.votes}
+              createdAt={post.createdAt}
+            />
+            <div className="horizontal-divider feed-divider"></div>
+          </div>
+        ))}
       </div>
       <div className="footer-bit"></div>
     </div>
