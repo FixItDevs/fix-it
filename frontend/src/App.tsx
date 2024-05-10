7b0c837a82105543bc84e7bb6dcd8b65445d6726
Pablo <pablito@me.com>
2024-05-10 13:16:53 +0100

    Added basic postContext and set provider in app.jsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FeedPage from "./pages/FeedPage/FeedPage";
 import Navbar from "./components/Navbar/Navbar";
+import PostContext from "./context/PostContext";
 
 function App() {
   return (
     <BrowserRouter>
+      <PostContext.Provider value={null}>
       <div id="app">
         <Navbar />
         {/* <div className="navbar-spacer"></div> */}
         <Routes>
           <Route path="/" element={<FeedPage />} />
         </Routes>
-      </div>
+        </div>
+      </PostContext.Provider>
     </BrowserRouter>
   );
 }
