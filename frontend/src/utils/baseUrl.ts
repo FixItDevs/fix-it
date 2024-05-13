8acc207d85f6709d9f3331f17a2e9825b02830f7
Pablo <pablito@me.com>
2024-05-13 14:08:37 +0100

    Added deployed backend url to basuurl file.

--- frontend/src/utils/baseUrl.ts
+++ frontend/src/utils/baseUrl.ts
@@ -1,6 +1,6 @@
 const baseUrl: string =
   process.env.NODE_ENV === "deploy"
-    ? "http://localhost:3000" // Yes, makes no sense now but will be needed for deployment
+    ? "https://fixit-backend-rqp5.onrender.com" 
     : "http://localhost:3000";
 
 export default baseUrl;
